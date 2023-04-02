b00bf2598d640ef8b6da30ad83e32c97f4bb5cb5
Arjun Pandey <54829402+parjun8840@users.noreply.github.com>
2023-04-02 10:01:46 +0900

    Create send_slack.sh

---
+++ send_slack.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+set -e
+usage() {
+  echo "usage: ${0##*/} -u <SLACK_URL> -t <TEXT>
+  exit 1
+}
+send_slack() {
+echo "Sending msg to slack"
+curl -k -X POST -H 'Content-type: application/json' --data "{ \"type\":\"mrkdwn\", \"text\": \"${text}\" }" "${slack_url}"
+}
+while getopts ":u:t:c:e:" opt; do
+
+ case $opt in
+   u) slack_url="$OPTARG"
+   ;;
+   t) text="$OPTARG"
+   ;;
+   \?) echo "Invalid option -$OPTARG" >&2 && usage
+   ;;
+  esac
+done
+
+if [ -z "$slack_url" ] || [ -z "$text" ]
+then
+usage
+fi
+
+send_slack
